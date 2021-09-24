0f8b0654480d8990683c3d6dff8d299bb2720c2f
ayundarynt <ayundarianti524@gmail.com>
2021-09-24 09:58:55 +0700

    update

--- database/migrations/2021_09_09_062951_create_request_complaint_customers_table.php
+++ database/migrations/2021_09_09_062951_create_request_complaint_customers_table.php
@@ -15,9 +15,9 @@ class CreateRequestComplaintCustomersTable extends Migration
     {
         Schema::create('request_complaint_customers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade')->onUpdate('cascade')->nullable();
+            $table->foreignId('company_id');
             $table->string('internal_eksternal');
-            $table->foreignId('pic_id')->constrained('pics')->onDelete('cascade')->onUpdate('cascade')->nullable();
+            $table->foreignId('pic');
             $table->string('vehicle');
             $table->dateTime('waktu_info');
             $table->longText('task');
